5e3c6a66f80623a4bc47cc658ba8940e19cde6d7
Vitor Cruz <vitorcruz2510@gmail.com>
2025-06-03 19:58:09 -0300

    classe-noavl

---
+++ src/NoAVL.java
@@ -0,0 +1,11 @@
+public class NoAVL {
+    String valor;
+    int altura;
+    NoAVL esquerda;
+    NoAVL direita;
+
+    public NoAVL(String valor){
+        this.valor = valor;
+        this.altura = 1;
+    }
+}
